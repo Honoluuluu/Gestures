0e3bcea5b494cf9d970b1e01e4950a25c661c16d
Van Luu <v.luu@hotmail.com>
2019-01-17 17:18:10 -0500

    Initial Commit

---
+++ Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gestures
+//
+//  Created by Van Luu on 2019-01-17.
+//  Copyright © 2019 Van Luu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gestures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gestures
+//
+//  Created by Van Luu on 2019-01-17.
+//  Copyright © 2019 Van Luu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
